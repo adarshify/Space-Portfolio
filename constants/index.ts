a5e9e2cee43253d6e71660e904e2b7d2d98c579c
adarshify <adarshchourasia6996@gmail.com>
2025-03-21 00:28:47 +0530

    creating Website

---
+++ constants/index.ts
@@ -0,0 +1,315 @@
+import { FaYoutube, FaGithub } from "react-icons/fa";
+import {
+  RxDiscordLogo,
+  RxGithubLogo,
+  RxInstagramLogo,
+  RxTwitterLogo,
+  RxLinkedinLogo,
+} from "react-icons/rx";
+
+export const SKILL_DATA = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+  
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Framer Motion",
+    image: "framer.png",
+    width: 80,
+    height: 80,
+  },
+  
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+] as const;
+
+export const SOCIALS = [
+  {
+    name: "Instagram",
+    icon: RxInstagramLogo,
+    link: "https://www.instagram.com/adarsh24__/",
+  },
+  {
+    name: "Github",
+    icon: FaGithub,
+    link: "https://github.com/adarshify",
+  },
+  {
+    name: "Twitter",
+    icon: RxTwitterLogo,
+    link: "https://x.com/adarshiify",
+  },
+] as const;
+
+export const FRONTEND_SKILL = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Material UI",
+    image: "mui.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+  
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const BACKEND_SKILL = [
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Express.js",
+    image: "express.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+  {
+    skill_name: "Firebase",
+    image: "firebase.png",
+    width: 55,
+    height: 55,
+  },
+ 
+  {
+    skill_name: "MySQL",
+    image: "mysql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Prisma",
+    image: "prisma.png",
+    width: 70,
+    height: 70,
+  },
+ 
+] as const;
+
+export const FULLSTACK_SKILL = [
+  
+  
+  {
+    skill_name: "Docker",
+    image: "docker.png",
+    width: 70,
+    height: 70,
+  },
+
+  
+] as const;
+
+export const OTHER_SKILL = [
+  
+] as const;
+
+export const PROJECTS = [
+  {
+    title: "Hygge - A ChatApp",
+    description:
+      'Developed a Real-time Chat Application using the MERN stack (MongoDB, Express, React, Node.js) to facilitate instant messaging',
+    image: "/projects/project-1.png",
+    link: "https://hygge-ffjz.onrender.com/",
+  },
+  {
+    title: "Flixer - Movie Recommendation Site",
+    description:
+      'A web application which allows users to share movies/tv shows among their connections and maintain their own personal watchlist.',
+    image: "/projects/project-2.png",
+    link: "https://example.com",
+  },
+  {
+    title: "Space Themed Website",
+    description:
+      'Embark on an interstellar journey with my "Space Themed Website", a mesmerizing space-themed website that invites you to explore the cosmic wonders beyond our world.',
+    image: "/projects/project-3.png",
+    link: "https://example.com",
+  },
+] as const;
+
+export const FOOTER_DATA = [
+  {
+    title: "Community",
+    data: [
+      
+      {
+        name: "GitHub",
+        icon: RxGithubLogo,
+        link: "https://github.com/adarshify",
+      },
+      {
+        name: "Discord",
+        icon: RxDiscordLogo,
+        link: "https://discord.com/adarshify",
+      },
+    ],
+  },
+  {
+    title: "Social Media",
+    data: [
+      {
+        name: "Instagram",
+        icon: RxInstagramLogo,
+        link: "https://www.instagram.com/adarsh24__/",
+      },
+      {
+        name: "Twitter",
+        icon: RxTwitterLogo,
+        link: "https://x.com/adarshiify",
+      },
+      {
+        name: "Linkedin",
+        icon: RxLinkedinLogo,
+        link: "https://www.linkedin.com/in/adarsh-chourasia6996/",
+      },
+    ],
+  },
+  {
+    title: "About",
+    data: [
+      {
+        name: "Become Sponsor",
+        icon: null,
+        link: "https://youtube.com",
+      },
+      {
+        name: "Learning about me",
+        icon: null,
+        link: "https://example.com",
+      },
+      {
+        name: "Contact Me",
+        icon: null,
+        link: "mailto:contact@adarshchourasia6996@gmail.com",
+      },
+    ],
+  },
+] as const;
+
+export const NAV_LINKS = [
+  {
+    title: "About me",
+    link: "#about-me",
+  },
+  {
+    title: "Skills",
+    link: "#skills",
+  },
+  {
+    title: "Projects",
+    link: "#projects",
+  },
+] as const;
+
+export const LINKS = {
+  sourceCode: "https://github.com/adarshify",
+};
